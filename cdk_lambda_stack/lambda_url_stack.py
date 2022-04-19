ea9085d482b0e73eea163187b9811b99548d2573
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2022-04-19 16:17:00 +0900

    fixed little issues

--- cdk_lambda_stack/lambda_url_stack.py
+++ cdk_lambda_stack/lambda_url_stack.py
@@ -1,6 +1,5 @@
 from aws_cdk import CfnOutput as Output
 from aws_cdk import CfnResource, Stack
-from aws_cdk import aws_apigateway as apigw
 from aws_cdk import aws_lambda as _lambda
 from aws_cdk import aws_lambda_python_alpha as _lambda_python
 from constructs import Construct
@@ -12,25 +11,21 @@ class LambdaStack(Stack):
 
         # in __init__ I like to initialize the infrastructure I will be creating
         self.example_lambda = None
-        self.gateway = None
-        # Additional useful infrastructure might include an S3 bucket,
-        # an EFS store, SQS queue, etc.
 
         self.build_infrastructure()
 
     def build_infrastructure(self):
         # For convenience, consolidate infrastructure construction
         self.build_lambda()
-        self.build_gateway()
 
     def build_lambda(self):
         self.example_lambda = _lambda_python.PythonFunction(
             scope=self,
             id="ExampleLambda",
             # entry points to the directory
-            entry="lambda_funcs/APILambda",
+            entry="lambda_funcs/LambdaURL",
             # index is the file name
-            index="API_lambda.py",
+            index="URL_lambda.py",
             # handler is the function entry point name in the lambda.py file
             handler="handler",
             runtime=_lambda.Runtime.PYTHON_3_9,
@@ -43,7 +38,7 @@ class LambdaStack(Stack):
             id="lambdaFuncUrl",
             type="AWS::Lambda::Url",
             properties={
-                "TargetFunctionArn": self.example_lambda.functionArn,
+                "TargetFunctionArn": self.example_lambda.function_arn,
                 "AuthType": "NONE",
                 "Cors": {"AllowOrigins": ["*"]},
             },
@@ -52,6 +47,7 @@ class LambdaStack(Stack):
         CfnResource(
             scope=self,
             id="funcURLPermission",
+            type="AWS::Lambda::Permission",
             properties={
                 "FunctionName": self.example_lambda.function_name,
                 "Principal": "*",
@@ -62,5 +58,5 @@ class LambdaStack(Stack):
         Output(
             scope=self,
             id="funcURLOutput",
-            value=cfnFuncUrl.get_att("FunctionUrl"),
+            value=str(cfnFuncUrl.get_att(attribute_name="FunctionUrl")),
         )
